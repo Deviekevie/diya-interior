186f9702506f5e855a4f7a7344145bab102d5b8e
Deviekevie <kaviyaramankaviyaraman@gmail.com>
2026-01-02 10:42:27 +0530

    Add contact

--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -15,6 +15,34 @@ const Footer = () => {
             <Link to="/contact" className="nav-link">Contact</Link>
           </div>
 
+          
+          <div className="footer-contact">
+
+            <p className="contact-item">
+              <span className="icon email-icon"></span>
+                <a href="mailto:diyamodular@gmail.com?subject=Interior%20Enquiry">
+                  diyamodular@gmail.com
+                </a>
+            </p>
+
+            <p className="contact-item">
+              <span className="icon phone-icon"></span>
+              <a href="tel:+919597882019">+91 95978 82019</a>
+            </p>
+
+            <p className="contact-item">
+              <span className="icon location-icon"></span>
+              <a
+                href="https://www.google.com/maps?q=No 360/ 2C1, CODEA PARK, Kurumbapalayam SSKulam, Tamil Nadu 641107"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                No 360/2C1, CODEA PARK, Kurumbapalayam SSKulam,Coimbatore, Tamil Nadu - 641107
+              </a>
+            </p>
+          </div>
+
+
         </div>
       </div>
     </footer>
